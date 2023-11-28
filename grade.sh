c0a8d7909c505ad2fe1bca12f24a65b0977b7fd8
soccerplayer6 <32963392+soccerplayer6@users.noreply.github.com>
2023-11-27 17:25:27 -0800

    Made progress on grader

--- grade.sh
+++ grade.sh
@@ -1,14 +1,33 @@
+#javac Server.java GradeServer.java
+#java GradeServer 6023
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
-
 rm -rf student-submission
 rm -rf grading-area
-
 mkdir grading-area
-
 git clone $1 student-submission
 echo 'Finished cloning'
+cd student-submission
+FILE=`find * -name ListExamples.java`
+FILENAME="ListExamples.java"
+echo $FILE
+if [[ $FILE == $FILENAME ]]
+then
+    echo "Correct file found"
+else
+    echo "File not found"
+    exit
+fi
+
+cd ..
+cp -r student-submission grading-area
+cd grading-area
+cd student-submission
+cp -r TestListExamples.java grading-area
+pwd
 
 
+javac -cp "$CPATH" *.java
+java -cp "$CPATH" org.junit.runner.JUnitCore TestListExamples
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
